93a816ecbedb902ea2e5a3bd3fdf683bc8a885f3
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-09-30 12:35:49 +0100

    Snake logic within Class now, for cleaner code

---
+++ snake.py
@@ -0,0 +1,50 @@
+from turtle import Turtle
+X_POS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+    
+    def create_snake(self):
+        for possition in X_POS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(possition)
+            self.segments.append(new_segment)
+    
+    def move(self):
+        for segment_number in range(2, 0, -1):
+            new_x = self.segments[segment_number - 1].xcor()
+            new_y = self.segments[segment_number - 1].ycor()
+            self.segments[segment_number].goto(new_x, new_y)
+
+        self.head.forward(MOVE_DISTANCE)
+    
+#Conditional statements to prevent the snake from going backwards on itself, just like the actual game
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
