8d9ba0d8b511755209b1cd38a56d72bd36f4fd05
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-09-30 18:32:02 +0100

    Updated snake class to allow for extending

--- snake.py
+++ snake.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-X_POS = [(0, 0), (-20, 0), (-40, 0)]
+STARTING_POS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
@@ -15,15 +15,21 @@ class Snake:
         self.head = self.segments[0]
     
     def create_snake(self):
-        for possition in X_POS:
+        for position in STARTING_POS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
             new_segment = Turtle("square")
-            new_segment.color("white")
+            new_segment.color("green")
             new_segment.penup()
-            new_segment.goto(possition)
+            new_segment.goto(position)
             self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
     
     def move(self):
-        for segment_number in range(2, 0, -1):
+        for segment_number in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[segment_number - 1].xcor()
             new_y = self.segments[segment_number - 1].ycor()
             self.segments[segment_number].goto(new_x, new_y)
