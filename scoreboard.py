4f39c600670217ece5b2447071c63c956e5caa50
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-10-05 20:05:46 +0100

    Opening external file to read and write high score

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        #open file with read permisions, and set the high score to the data provided
+        with open("/Users/emre/Documents/Code/100DaysOfPython/Day11-20/Day20/data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,16 +18,26 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
-
+        self.clear()
+        self.write(f"Score: {self.score} | High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score: #Adds the score to high score if it is greater
+            self.high_score = self.score
+            #saves the latest high score to the data file with write permisions
+            with open("/Users/emre/Documents/Code/100DaysOfPython/Day11-20/Day20/data.txt", "w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0 #Reset score
+        self.update_scoreboard()
+    
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
 
         
